8156219485f6865562861d930755a47e1b290f9a
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-05-17 19:47:34 -0300

    Add project files.

---
+++ AwesomeDevEvents/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEvents.Entities;
+
+namespace AwesomeDevEvents.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
